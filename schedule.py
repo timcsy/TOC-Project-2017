81729042b39a9ef2804f9eb9f76565bd5ed9a5b7
timcsy <timocsy@yahoo.com.tw>
2017-12-30 19:14:11 +0800

    test

--- schedule.py
+++ schedule.py
@@ -13,6 +13,17 @@ import sched
 # 				action()
 # 				self.bot.send_message(chat_id=236304646, text=s)
 
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
+
 class MainTask(threading.Thread):
     def __init__(self, bot):
         threading.Thread.__init__(self)
@@ -22,3 +33,10 @@ class MainTask(threading.Thread):
         while True:
             s = input('sent to client > ')
             self.bot.send_message(chat_id=236304646, text=s)
+						button_list = [
+								InlineKeyboardButton("col1", callback_data='col1callbacktest'),
+								InlineKeyboardButton("col2", callback_data='col2callbacktest'),
+								InlineKeyboardButton("row 2", callback_data='row2callbacktest')
+						]
+						reply_markup = InlineKeyboardMarkup(util.build_menu(button_list, n_cols=2))
+						bot.send_message(chat_id=236304646, text="A two-column menu", reply_markup=reply_markup)
